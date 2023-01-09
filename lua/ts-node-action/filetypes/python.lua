d570b039ad78f49e54d7350fea08214d424c8bdb
bekaboo <18127878294@qq.com>
2023-01-09 17:36:28 -0600

    Feat: add operator toggler for python

--- lua/ts-node-action/filetypes/python.lua
+++ lua/ts-node-action/filetypes/python.lua
@@ -13,10 +13,11 @@ local boolean_override = {
 }
 
 return {
-  ["dictionary"]    = actions.toggle_multiline(padding),
-  ["list"]          = actions.toggle_multiline(padding),
-  ["argument_list"] = actions.toggle_multiline(padding),
-  ["parameters"]    = actions.toggle_multiline(padding),
-  ["true"]          = actions.toggle_boolean(boolean_override),
-  ["false"]         = actions.toggle_boolean(boolean_override),
+  ["dictionary"]          = actions.toggle_multiline(padding),
+  ["list"]                = actions.toggle_multiline(padding),
+  ["argument_list"]       = actions.toggle_multiline(padding),
+  ["parameters"]          = actions.toggle_multiline(padding),
+  ["true"]                = actions.toggle_boolean(boolean_override),
+  ["false"]               = actions.toggle_boolean(boolean_override),
+  ["comparison_operator"] = actions.toggle_operator(),
 }
