48f8bf84a6f8fe7cb353ea23a4fcfe4d304f467c
custompro98 <mitchjoa@gmail.com>
2023-01-12 12:03:26 -0500

    Add support for typescript

--- lua/ts-node-action/init.lua
+++ lua/ts-node-action/init.lua
@@ -64,6 +64,7 @@ M.node_actions = {
   json       = require("ts-node-action/filetypes/json"),
   ruby       = require("ts-node-action/filetypes/ruby"),
   javascript = require("ts-node-action/filetypes/javascript"),
+  typescript = require("ts-node-action/filetypes/typescript"),
   python     = require("ts-node-action/filetypes/python"),
 }
 
