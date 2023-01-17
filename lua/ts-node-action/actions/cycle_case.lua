fda0b78bdc0a01b5e52f896c745c9afa27d6a3eb
Cameron <Alleyria@gmail.com>
2023-01-17 13:03:37 +0100

    Reimplement cycle_case to allow user specified formats, or different order of built-in formats

--- lua/ts-node-action/actions/cycle_case.lua
+++ lua/ts-node-action/actions/cycle_case.lua
@@ -1,97 +1,117 @@
 local helpers = require("ts-node-action.helpers")
 
-local snake_case = {
-  check = function(text)
-    return (string.find(text, "_")
-        and string.sub(text, 1, 1) == string.sub(text, 1, 1):lower())
-        or text:lower() == text
-  end,
+-- API Notes:
+-- Every format is a table that implements the following three keys:
+-- - pattern
+-- - apply
+-- - standardize
+--
+-- # Pattern
+-- A Lua pattern (string) that matches the format
+--
+-- # Apply
+-- A function that takes a _table_ of standardized strings as it's argument, and returns a _string_ in the format
+--
+-- # Standardize
+-- A function that takes a _string_ in this format, and returns a table of strings, all lower case, no special chars.
+-- ie: standardize("ts_node_action") -> { "ts", "node", "action" }
+--     standardize("tsNodeAction")   -> { "ts", "node", "action" }
+--     standardize("TsNodeAction")   -> { "ts", "node", "action" }
+--     standardize("TS_NODE_ACTION") -> { "ts", "node", "action" }
+--
+-- NOTE: The order of formats can be important, as some identifiers are the same for multiple formats.
+--   Take the string 'action' for example. This is a match for both snake_case _and_ camel_case. It's
+--   therefore important to place a format between those two so we can correcly change the string.
 
-  transform = function(tbl)
-    return string.lower(table.concat(tbl, "_"))
-  end,
+local format_table = {
+  snake_case = {
+    pattern = "^%l+[%l_]*$",
+    apply = function(tbl)
+      return string.lower(table.concat(tbl, "_"))
+    end,
+    standardize = function(text)
+      return vim.split(string.lower(text), "_", { trimempty = true })
+    end
+  },
+  camel_case = {
+    pattern = "^%l+[%u%l]*$",
+    apply = function(tbl)
+      local tmp = vim.tbl_map(function(word)
+        return word:gsub("^.", string.upper)
+      end, tbl)
+      local value, _ = table.concat(tmp, ""):gsub("^.", string.lower)
+      return value
+    end,
+    standardize = function(text)
+      return vim.split(
+        text:gsub(".%f[%l]", " %1"):gsub("%l%f[%u]", "%1 "):gsub("^.", string.upper), " ",
+        { trimempty = true }
+      )
+    end
+  },
+  pascal_case = {
+    pattern = "^%u%l+[%u%l]*$",
+    apply = function(tbl)
+      local value, _ = table.concat(
+        vim.tbl_map(function(word) return word:gsub("^.", string.upper) end, tbl), ""
+      )
+      return value
+    end,
+    standardize = function(text)
+      return vim.split(
+        text:gsub(".%f[%l]", " %1"):gsub("%l%f[%u]", "%1 "):gsub("^.", string.upper), " ",
+        { trimempty = true }
+      )
+    end
+  },
+  screaming_snake_case = {
+    pattern = "^%u+[%u_]*$",
+    apply = function(tbl)
+      local value, _ = table.concat(
+        vim.tbl_map(function(word) return word:upper() end, tbl), "_"
+      )
 
-  standardize = function(text)
-    return vim.split(string.lower(text), "_", { trimempty = true })
-  end
+      return value
+    end,
+    standardize = function(text)
+      return vim.split(string.lower(text), "_", { trimempty = true })
+    end
+  }
 }
 
-local camel_case = {
-  transform = function(tbl)
-    local tmp = vim.tbl_map(function(word)
-      return word:gsub("^.", string.upper)
-    end, tbl)
-    local value, _ = table.concat(tmp, ""):gsub("^.", string.lower)
-    return value
-  end,
+local function check_pattern(text, pattern)
+  return not not string.find(text, pattern)
+end
 
-  standardize = function(text)
-    local tmp = text:gsub(".%f[%l]", " %1"):gsub("%l%f[%u]", "%1 "):gsub("^.", string.upper)
-    return vim.split(tmp, " ", { trimempty = true })
-  end
-}
+local default_formats = { "snake_case", "pascal_case", "screaming_snake_case", "camel_case" }
 
-local pascal_case = {
-  check = function(text)
-    return string.sub(text, 1, 1) == string.sub(text, 1, 1):upper()
-  end,
+return function(user_formats)
+  user_formats = user_formats or default_formats
 
-  transform = function(tbl)
-    local tmp = vim.tbl_map(function(word)
-      return word:gsub("^.", string.upper)
-    end, tbl)
-
-    local value, _ = table.concat(tmp, "")
-
-    return value
-  end,
-
-  standardize = function(text)
-    local tmp = text:gsub(".%f[%l]", " %1"):gsub("%l%f[%u]", "%1 "):gsub("^.", string.upper)
-    return vim.split(tmp, " ", { trimempty = true })
-  end
-}
-
-local screaming_snake_case = {
-  check = function(text)
-    return string.sub(text, 1, 2) == string.sub(text, 1, 2):upper()
-  end,
-
-  transform = function(tbl)
-    local tmp = vim.tbl_map(function(word)
-      return word:upper()
-    end, tbl)
-
-    local value, _ = table.concat(tmp, "_")
-
-    return value
-  end,
-
-  standardize = function(text)
-    return vim.split(string.lower(text), "_", { trimempty = true })
-  end
-}
-
-return function()
-  return function(node)
-    local text = helpers.node_text(node)
-    local standardize
-    local transform
-
-    if snake_case.check(text) then
-      standardize = snake_case.standardize
-      transform   = pascal_case.transform
-    elseif screaming_snake_case.check(text) then
-      standardize = screaming_snake_case.standardize
-      transform   = camel_case.transform
-    elseif pascal_case.check(text) then
-      standardize = pascal_case.standardize
-      transform   = screaming_snake_case.transform
-    else
-      standardize = camel_case.standardize
-      transform   = snake_case.transform
+  local formats = {}
+  for _, format in ipairs(user_formats) do
+    if type(format) == "string" then
+      format = format_table[format]
     end
 
-    return transform(standardize(text))
+    if format then
+      table.insert(formats, format)
+    else
+      print("TS:NodeAction:CycleCase - Format '" .. format .. "' is invalid")
+    end
+  end
+
+  return function(node)
+    local text = helpers.node_text(node)
+
+    for i, format in ipairs(formats) do
+      if check_pattern(text, format.pattern) then
+        local next_i      = i + 1 > #formats and 1 or i + 1
+        local apply       = formats[next_i].apply
+        local standardize = format.standardize
+
+        return apply(standardize(text))
+      end
+    end
   end
 end
