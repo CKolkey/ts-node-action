49d37f95a465448f5491ed8e854d9455a3887e06
Cameron <Alleyria@gmail.com>
2023-01-18 19:49:52 +0100

    Filetype Support! Adds a bunch more node types for Javascript, and maps javascript to javascriptreact, typescriptreact, svelte

--- lua/ts-node-action/filetypes/javascript.lua
+++ lua/ts-node-action/filetypes/javascript.lua
@@ -1,5 +1,16 @@
 local actions = require("ts-node-action.actions")
 
+local operators = {
+  ["!="]  = "==",
+  ["!=="] = "===",
+  ["=="]  = "!=",
+  ["==="] = "!==",
+  [">"]   = "<",
+  ["<"]   = ">",
+  [">="]  = "<=",
+  ["<="]  = ">=",
+}
+
 local padding = {
   [","] = "%s ",
   [":"] = "%s ",
@@ -8,8 +19,14 @@ local padding = {
 }
 
 return {
+  ["property_identifier"] = actions.cycle_case(),
+  ["binary_expression"]   = actions.toggle_operator(operators),
   ["object"]              = actions.toggle_multiline(padding),
   ["array"]               = actions.toggle_multiline(padding),
   ["statement_block"]     = actions.toggle_multiline(padding),
-  ["property_identifier"] = actions.cycle_case(),
+  ["object_pattern"]      = actions.toggle_multiline(padding),
+  ["object_type"]         = actions.toggle_multiline(padding),
+  ["formal_parameters"]   = actions.toggle_multiline(padding),
+  ["argument_list"]       = actions.toggle_multiline(padding),
+  ["method_parameters"]   = actions.toggle_multiline(padding),
 }
