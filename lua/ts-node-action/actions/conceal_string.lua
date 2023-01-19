37bd1b7bd27dc435ca07a777a6235511730d01ad
Cameron <Alleyria@gmail.com>
2023-01-19 10:54:00 +0100

    Slight refactor to conceal_string()

--- lua/ts-node-action/actions/conceal_string.lua
+++ lua/ts-node-action/actions/conceal_string.lua
@@ -1,8 +1,15 @@
 local namespace = vim.api.nvim_create_namespace('ts_node_action_conceal')
 
+local function set_win_conceal(conceal_level)
+  local win_id = vim.api.nvim_get_current_win()
+  if vim.wo[win_id].conceallevel < 1 then
+    vim.wo[win_id].conceallevel = conceal_level
+  end
+end
+
 return function(conceal_char, conceal_level)
+  set_win_conceal(conceal_level or 1)
   conceal_char = conceal_char or ""
-  vim.wo[vim.api.nvim_get_current_win()].conceallevel = conceal_level or 1
 
   return function(node)
     local start_row, start_col, end_row, end_col = node:range()
