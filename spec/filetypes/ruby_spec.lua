ba1a4e3a157b9330a132d6a2a1c220a449e44030
Cameron <Alleyria@gmail.com>
2023-02-12 14:13:00 +0100

    Polish

--- spec/filetypes/ruby_spec.lua
+++ spec/filetypes/ruby_spec.lua
@@ -4,15 +4,15 @@ local Helper = SpecHelper:new("ruby")
 
 describe("integer", function()
   it("adds underscores to long int", function()
-    assert.are.same("1_000_000", Helper:call("1000000"))
+    assert.are.same({ "1_000_000" }, Helper:call("1000000"))
   end)
 
   it("removes underscores from long int", function()
-    assert.are.same("1000000", Helper:call("1_000_000"))
+    assert.are.same({ "1000000" }, Helper:call("1_000_000"))
   end)
 
   it("doesn't change ints less than four places", function()
-    assert.are.same("100", Helper:call("100"))
+    assert.are.same({ "100" }, Helper:call("100"))
   end)
 end)
 
@@ -30,7 +30,7 @@ describe("if_condition", function()
 
   it("collapses from three lines to one", function()
     assert.are.same(
-      [[puts "hello" if greet?]],
+      { [[puts "hello" if greet?]] },
       Helper:call({
         [[if greet?]],
         [[  puts "hello"]],
--- spec/spec_helper.lua
+++ spec/spec_helper.lua
@@ -1,82 +1,113 @@
 local Buffer = {}
 Buffer.__index = Buffer
 
+-- Creates a new Buffer object for testing. Custom buf options can be passed into constructor.
 -- @param lang string
--- @return self
-function Buffer:new(lang)
+-- @param user_opts table
+-- @return Buffer
+function Buffer:new(lang, buf_opts)
   local buffer = {}
   buffer.lang  = lang
+  buffer.opts  = vim.tbl_extend(
+    "keep",
+    { filetype = lang, indentexpr = "nvim_treesitter#indent()" },
+    buf_opts or {}
+  )
 
   return setmetatable(buffer, self)
 end
 
-function Buffer:setup(cursor)
+-- @return Buffer
+function Buffer:setup()
   self.handle = vim.api.nvim_create_buf(false, true)
-
   vim.treesitter.start(self.handle, self.lang)
-  vim.api.nvim_buf_set_option(self.handle, "filetype", self.lang)
-  vim.api.nvim_buf_set_option(self.handle, "indentexpr", "nvim_treesitter#indent()")
 
-  require("nvim-treesitter.ts_utils").get_node_at_cursor = function()
+  for key, value in pairs(self.opts) do
+    vim.api.nvim_buf_set_option(self.handle, key, value)
+  end
+
+  return self
+end
+
+-- Fakes cursor location by just returning the node at where the cursor should be
+-- 1-indexed { row, col }
+--
+-- @param pos table
+-- @return Buffer
+function Buffer:set_cursor(pos)
+  local fake_get_node_at_cursor = function()
     return vim.treesitter.get_parser(self.handle, self.lang)
         :parse()[1]
         :root()
-        :named_descendant_for_range(cursor[1] - 1, cursor[2], cursor[1] - 1, cursor[2])
+        :named_descendant_for_range(pos[1] - 1, pos[2], pos[1] - 1, pos[2])
   end
+
+  require("nvim-treesitter.ts_utils").get_node_at_cursor = fake_get_node_at_cursor
+
+  return self
 end
 
+-- @param text string|table
+-- @return Buffer
 function Buffer:write(text)
   if type(text) ~= "table" then
     text = { text }
   end
 
   vim.api.nvim_buf_set_lines(self.handle, 0, -1, false, text)
+  return self
 end
 
+-- @return table
 function Buffer:read()
   return vim.api.nvim_buf_get_lines(self.handle, 0, -1, false)
 end
 
+-- @return nil
 function Buffer:teardown()
   vim.api.nvim_buf_delete(self.handle, { force = true })
 end
 
+-- @return Buffer
 function Buffer:run_action()
   vim.api.nvim_buf_call(self.handle, require("ts-node-action").node_action)
+  return self
 end
 
--- SpecHelper
+-- SpecHelper - A general wrapper, available in the global scope, for test related helpers
 local SpecHelper = {}
 SpecHelper.__index = SpecHelper
 
 _G.SpecHelper = SpecHelper
 
--- Builds language helper for lang
+-- Builds language helper. Custom buffer opts can also be set on a per-filetype basis.
 -- @param lang string
--- @return self
-function SpecHelper:new(lang)
-  local language   = {}
-  language.lang    = lang
-  language.actions = require("ts-node-action.filetypes")[lang]
+-- @param buf_opts table|nil
+-- @return SpecHelper
+function SpecHelper:new(lang, buf_opts)
+  local language    = {}
+  language.lang     = lang
+  language.buf_opts = buf_opts or {}
 
   return setmetatable(language, self)
 end
 
 -- Runs full integration test for text
+-- Cursor (pos) is 1-indexed, { row, col }. Defaults to first line, first col if empty
+-- Returns full buffer text as a table, one string per line.
+--
 -- @param text string|table
--- @param cursor table
--- @return table|string
-function SpecHelper:call(text, cursor)
-  local buf = Buffer:new(self.lang)
-  buf:setup(cursor or { 1, 1 }) -- row, col
-  buf:write(text)
-  buf:run_action()
-  local result = buf:read()
-  buf:teardown()
+-- @param pos table|nil
+-- @return table
+function SpecHelper:call(text, pos)
+  local buffer = Buffer:new(self.lang, self.buf_opts)
+  local result = buffer:setup()
+      :set_cursor(pos or { 1, 1 })
+      :write(text)
+      :run_action()
+      :read()
 
-  if result[2] then
-    return result
-  else
-    return result[1]
-  end
+  buffer:teardown()
+
+  return result
 end
