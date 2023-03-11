8c627ddab2e1cdb3c5cbf6c13f467f96cef0f8ea
Cameron <Alleyria@gmail.com>
2023-03-11 18:09:17 +0100

    Update julia_spec.lua

--- spec/filetypes/julia_spec.lua
+++ spec/filetypes/julia_spec.lua
@@ -3,9 +3,7 @@ dofile("spec/spec_helper.lua")
 local Helper = SpecHelper.new("julia", { shiftwidth = 2 })
 
 describe("operator", function()
-
-	it("toggles '>=' into '<='", function()
-		assert.are.same({ "i <= 8" }, Helper:call({ "i >= 8" }, { 1, 3 }))
-	end)
-
+  it("toggles '>=' into '<='", function()
+    assert.are.same({ "i <= 8" }, Helper:call({ "i >= 8" }, { 1, 3 }))
+  end)
 end)
