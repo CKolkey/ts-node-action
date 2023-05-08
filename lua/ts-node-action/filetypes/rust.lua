db23cd974647fa2e0514336c32a0bd6f0948fcbb
Alex Walker <alex.walker@crossbar.org>
2023-05-07 19:46:55 -0500

    feat: support for rust

--- lua/ts-node-action/filetypes/rust.lua
+++ lua/ts-node-action/filetypes/rust.lua
@@ -1,26 +1,86 @@
 local actions = require("ts-node-action.actions")
 
+local boolean_override = {
+  ["true"] = "false",
+  ["false"] = "true",
+}
+
+local operators = {
+  -- assignment
+  ["+="] = "-=",
+  ["-="] = "+=",
+  ["%="] = "/=",
+  ["/="] = "%=",
+  -- bitwise assignment
+  ["&="] = "|=",
+  ["|="] = "^=",
+  ["^="] = "&=",
+  -- comparison
+  ["!="] = "==",
+  ["=="] = "!=",
+  [">"]  = "<",
+  ["<"]  = ">",
+  [">="] = "<=",
+  ["<="] = ">=",
+  -- shift
+  ["<<"] = ">>",
+  [">>"] = "<<",
+  -- shift assignment
+  [">>="] = "<<=",
+  ["<<="] = ">>=",
+  -- arithmetic
+  ["+"] = "-",
+  ["-"] = "+",
+  ["*"] = "/",
+  ["/"] = "*",
+  -- bitwise
+  ["|"] = "&",
+  ["&"] = "|",
+  -- logical
+  ["||"] = "&&",
+  ["&&"] = "||",
+}
+
 local padding = {
-  [","]  = "%s ",
-  [":"]  = "%s ",
   ["{"]  = "%s ",
-  ["=>"] = " %s ",
-  ["="]  = " %s ",
-  ["}"]  = " %s",
-  ["+"]  = " %s ",
-  ["-"]  = " %s ",
-  ["*"]  = " %s ",
-  ["/"]  = " %s ",
+  ["}"]  = {
+    " %s",
+    ["{"] = "%s",
+    [","] = "%s",
+    [";"] = "%s",
+    ["prev_nil"] = "%s",
+  },
+  ["as"] = " %s ",
+  ["in"] = { " %s ", ["prev_nil"] = "%s ", },
+  [":"]  = "%s ",
+  [";"]  = "%s ",
+  [","]  = "%s ",
+}
+local padding_compact = {
+  ["{"]  = "%s",
+  ["}"]  = "%s",
+  [","]  = "%s ",
+}
+
+local uncollapsible = {
+  ["string_literal"]   = true,
+  ["macro_invocation"] = true,
+  ["macro"]            = true,
+  ["for_expression"]   = true,
+  ["range_expression"] = true,
+  ["line_comment"]     = true,
 }
 
 return {
-  ["field_declaration_list"] = actions.toggle_multiline(padding),
-  ["parameters"]             = actions.toggle_multiline(padding),
-  ["enum_variant_list"]      = actions.toggle_multiline(padding),
-  ["block"]                  = actions.toggle_multiline(padding),
-  ["array_expression"]       = actions.toggle_multiline(padding),
-  ["tuple_expression"]       = actions.toggle_multiline(padding),
-  ["tuple_pattern"]          = actions.toggle_multiline(padding),
-  ["boolean_literal"]        = actions.toggle_boolean(),
-  ["integer_literal"]        = actions.toggle_int_readability(),
+  ["boolean_literal"]          = actions.toggle_boolean(boolean_override),
+  ["integer_literal"]          = actions.toggle_int_readability(),
+  ["binary_expression"]        = actions.toggle_operator(operators),
+  ["compound_assignment_expr"] = actions.toggle_operator(operators),
+  ["block"]                    = actions.toggle_multiline(padding, uncollapsible),
+  ["use_list"]                 = actions.toggle_multiline(padding_compact, uncollapsible),
+  ["array_expression"]         = actions.toggle_multiline(padding, uncollapsible),
+  ["tuple_expression"]         = actions.toggle_multiline(padding, uncollapsible),
+  ["arguments"]                = actions.toggle_multiline(padding, uncollapsible),
+  ["field_initializer_list"]   = actions.toggle_multiline(padding, uncollapsible),
+  ["field_declaration_list"]   = actions.toggle_multiline(padding, uncollapsible),
 }
---
+++ spec/filetypes/rust_spec.lua
@@ -0,0 +1,325 @@
+dofile("spec/spec_helper.lua")
+
+local Helper = SpecHelper.new("rust", { shiftwidth = 4 })
+
+describe("boolean", function()
+    it("toggles 'true' and 'false'", function()
+        assert.are.same({ "let i = true;" }, Helper:call({ "let i = false;" }, { 1, 9 }))
+        assert.are.same({ "let i = false;" }, Helper:call({ "let i = true;" }, { 1, 9 }))
+    end)
+end)
+
+describe("friendly integers", function()
+    it("1 million to friendly", function()
+        assert.are.same({ "x = 1000000" }, Helper:call({
+            "x = 1_000_000",}, { 1, 5 }))
+    end)
+
+    it("1 million to unfriendly", function()
+        assert.are.same({ "x = 1_000_000" }, Helper:call({
+            "x = 1000000",}, { 1, 5 }))
+    end)
+end)
+
+describe("operator", function()
+    -- assignment
+    it("toggles '-=' and '+='", function()
+        assert.are.same({ "i += 8" }, Helper:call({ "i -= 8" }, { 1, 3 }))
+        assert.are.same({ "i -= 8" }, Helper:call({ "i += 8" }, { 1, 3 }))
+    end)
+
+    it("toggles '/=' and '%='", function()
+        assert.are.same({ "i %= 8" }, Helper:call({ "i /= 8" }, { 1, 3 }))
+        assert.are.same({ "i /= 8" }, Helper:call({ "i %= 8" }, { 1, 3 }))
+    end)
+
+    -- bitwise assignment
+    it("toggles '&=' and '|=' and '^='", function()
+        assert.are.same({ "i |= 8" }, Helper:call({ "i &= 8" }, { 1, 3 }))
+        assert.are.same({ "i ^= 8" }, Helper:call({ "i |= 8" }, { 1, 3 }))
+        assert.are.same({ "i &= 8" }, Helper:call({ "i ^= 8" }, { 1, 3 }))
+    end)
+
+    -- comparison
+    it("toggles '==' and '!='", function()
+        assert.are.same({ "i == 8" }, Helper:call({ "i != 8" }, { 1, 3 }))
+        assert.are.same({ "i != 8" }, Helper:call({ "i == 8" }, { 1, 3 }))
+    end)
+
+    it("toggles '>' and '<'", function()
+        assert.are.same({ "i > 8" }, Helper:call({ "i < 8" }, { 1, 3 }))
+        assert.are.same({ "i < 8" }, Helper:call({ "i > 8" }, { 1, 3 }))
+    end)
+
+    it("toggles '<= and '>='", function()
+        assert.are.same({ "i <= 8" }, Helper:call({ "i >= 8" }, { 1, 3 }))
+        assert.are.same({ "i >= 8" }, Helper:call({ "i <= 8" }, { 1, 3 }))
+    end)
+
+    -- shift
+    it("toggles '<<' and '>>'", function()
+        assert.are.same({ "i << 8" }, Helper:call({ "i >> 8" }, { 1, 3 }))
+        assert.are.same({ "i >> 8" }, Helper:call({ "i << 8" }, { 1, 3 }))
+    end)
+
+    -- shift assignment
+    it("toggles '<<=' and '>>='", function()
+        assert.are.same({ "i >>= 8" }, Helper:call({ "i <<= 8" }, { 1, 3 }))
+        assert.are.same({ "i <<= 8" }, Helper:call({ "i >>= 8" }, { 1, 3 }))
+    end)
+
+    -- arithmetic
+    it("toggles '+' and '-'", function()
+        assert.are.same({ "i + 8" }, Helper:call({ "i - 8" }, { 1, 3 }))
+        assert.are.same({ "i - 8" }, Helper:call({ "i + 8" }, { 1, 3 }))
+    end)
+
+    it("toggles '*' and '/'", function()
+        assert.are.same({ "i * 8" }, Helper:call({ "i / 8" }, { 1, 3 }))
+        assert.are.same({ "i / 8" }, Helper:call({ "i * 8" }, { 1, 3 }))
+    end)
+
+    -- bitwise
+    it("toggles '|' and '&'", function()
+        assert.are.same({ "i | 8" }, Helper:call({ "i & 8" }, { 1, 3 }))
+        assert.are.same({ "i & 8" }, Helper:call({ "i | 8" }, { 1, 3 }))
+    end)
+
+    -- logical
+    it("toggles '||' and '&&'", function()
+        assert.are.same({ "i || 8" }, Helper:call({ "i && 8" }, { 1, 3 }))
+        assert.are.same({ "i && 8" }, Helper:call({ "i || 8" }, { 1, 3 }))
+    end)
+end)
+
+describe("toggle_multiline", function()
+    it("block", function()
+        assert.are.same(
+            {
+                "fn main() {",
+                "    println!(\"main:\\tmodule '{}', file '{}'\", module_path!(), file!());",
+                "    submod::hi();",
+                "}"
+            },
+            Helper:call(
+                {
+                    "fn main() { println!(\"main:\\tmodule '{}', file '{}'\", module_path!(), file!()); submod::hi();  }"
+                },
+                { 1, 11 }
+            )
+        )
+
+        assert.are.same(
+            {
+                "fn main() { println!(\"main:\\tmodule '{}', file '{}'\", module_path!(), file!()); submod::hi();  }"
+            },
+            Helper:call(
+                {
+                    "fn main() {",
+                    "    println!(\"main:\\tmodule '{}', file '{}'\", module_path!(), file!());",
+                    "    submod::hi();",
+                    "}"
+                },
+                { 1, 11 }
+            )
+        )
+    end)
+
+    it("use_list", function()
+        assert.are.same(
+            {
+                "use std::collections::{",
+                "    HashMap,",
+                "    HashSet",
+                "};",
+            },
+            Helper:call(
+                {
+                    "use std::collections::{HashMap, HashSet};",
+                },
+                { 1, 23 }
+            )
+        )
+
+        assert.are.same(
+            {
+                "use std::collections::{HashMap, HashSet};",
+            },
+            Helper:call(
+                {
+                    "use std::collections::{",
+                    "    HashMap,",
+                    "    HashSet",
+                    "};",
+                },
+                { 1, 23 }
+            )
+        )
+    end)
+
+    it("array_expression", function()
+        assert.are.same(
+            {
+                "let bytes: [u8; 3] = [",
+                "    1,",
+                "    2,",
+                "    3",
+                "];",
+            },
+            Helper:call(
+                {
+                    "let bytes: [u8; 3] = [1, 2, 3];"
+                },
+                { 1, 22 }
+            )
+        )
+
+        assert.are.same(
+            {
+                "let bytes: [u8; 3] = [1, 2, 3];",
+            },
+            Helper:call(
+                {
+                    "let bytes: [u8; 3] = [",
+                    "    1,",
+                    "    2,",
+                    "    3",
+                    "];",
+                },
+                { 1, 22 }
+            )
+        )
+    end)
+
+    it("arguments", function()
+        assert.are.same(
+            {
+                "list.insert_at_ith(",
+                "    0,",
+                "    first_value",
+                ");"
+            },
+            Helper:call(
+                {
+                    "list.insert_at_ith(0, first_value);",
+                },
+                { 1, 19 }
+            )
+        )
+
+        assert.are.same(
+            {
+                "list.insert_at_ith(0, first_value);",
+            },
+            Helper:call(
+                {
+                    "list.insert_at_ith(",
+                    "    0,",
+                    "    first_value",
+                    ");",
+                },
+                { 1, 19 }
+            )
+        )
+    end)
+
+    it("tuple_expression", function()
+        assert.are.same(
+            {
+                "let foo = (",
+                "    0.0,",
+                "    4.5",
+                ")"
+            },
+            Helper:call(
+                {
+                    "let foo = (0.0, 4.5)"
+                },
+                { 1, 11 }
+            )
+        )
+
+        assert.are.same(
+            {
+                "let foo = (0.0, 4.5)",
+            },
+            Helper:call(
+                {
+                    "let foo = (",
+                    "    0.0,",
+                    "    4.5",
+                    ")",
+                },
+                { 1, 11 }
+            )
+        )
+    end)
+
+    it("field_initializer_list", function()
+        assert.are.same(
+            {
+                "Foo {",
+                "    a: 1,",
+                "    b: 2,",
+                "    c: 3",
+                "}",
+            },
+            Helper:call(
+                {
+                    "Foo { a: 1, b: 2, c: 3 }",
+                },
+                { 1, 5 }
+            )
+        )
+
+        assert.are.same(
+            {
+                "Foo { a: 1, b: 2, c: 3 }",
+            },
+            Helper:call(
+                {
+                    "Foo {",
+                    "    a: 1,",
+                    "    b: 2,",
+                    "    c: 3",
+                    "}",
+                },
+                { 1, 5 }
+            )
+        )
+    end)
+
+    it("field_declaration_list", function()
+        assert.are.same(
+            {
+                "struct Foo {",
+                "    a: i32,",
+                "    b: i32,",
+                "    c: i32",
+                "}",
+            },
+            Helper:call(
+                {
+                    "struct Foo { a: i32, b: i32, c: i32 }",
+                },
+                { 1, 12 }
+            )
+        )
+
+        assert.are.same(
+            {
+                "struct Foo { a: i32, b: i32, c: i32 }",
+            },
+            Helper:call(
+                {
+                    "struct Foo {",
+                    "    a: i32,",
+                    "    b: i32,",
+                    "    c: i32",
+                    "}",
+                },
+                { 1, 12 }
+            )
+        )
+    end)
+end)
